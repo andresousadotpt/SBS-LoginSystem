2086a893e1de964988999c5b412cb30aef4958e5
André Sousa <andre.sousa@andresousa.pt>
2023-01-17 21:50:35 +0000

    Backend startup project

---
+++ SBS-Backend/src/main/java/pt/andresousa/SBSBackend/SbsBackendApplication.java
@@ -0,0 +1,13 @@
+package pt.andresousa.SBSBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbsBackendApplication.class, args);
+	}
+
+}
---
+++ SBS-Backend/src/test/java/pt/andresousa/SBSBackend/SbsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.andresousa.SBSBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
